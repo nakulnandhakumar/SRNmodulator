cdb142cf59c3fb00a19e5626ea27e6b363174793
nakulnandhakumar <nakul@nandhakumar.net>
2026-03-25 10:02:21 -0700

    minor changes

--- code_verification/code_verification.py
+++ code_verification/code_verification.py
@@ -2,6 +2,10 @@ import sys
 sys.path.append(r"C:\Program Files\Lumerical\v202\api\python")
 import lumapi # pyright: ignore[reportMissingImports]
 
+# ============================================================
+# Lumerical session management
+# ============================================================
+
 charge = lumapi.DEVICE(
     hide=False, 
     project=r"./lumerical/electrostatics/modulator_electrostatics.ldev"

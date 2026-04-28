1ed982fca5f8ed305fab12f8f30ba55adff1979f
nakulnandhakumar <nakul@nandhakumar.net>
2026-04-27 20:32:19 -0700

    sweep bigger range to show plot for slides

--- ring_resonator/ring_filter_buffer_thickness.py
+++ ring_resonator/ring_filter_buffer_thickness.py
@@ -18,7 +18,7 @@ ring_supermode = lumapi.MODE(
 with open(r"./lumerical/mode/ring_filter_waveguide_run.lsf") as f:
     eta_sweep_script = f.read()
     
-t_buffer_values = np.linspace(185e-9, 195e-9, 5)  # Sweep buffer thickness from 0 to 100 nm
+t_buffer_values = np.linspace(120e-9, 200e-9, 25)  # Sweep buffer thickness from 0 to 100 nm
 
 material_params = {
     "core_material_mode": "SRN 3.1 (Silicon Rich Nitride)",

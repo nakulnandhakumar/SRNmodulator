e562b65b76871b39547aa4a48849edbfe54ab79b
nakulnandhakumar <nakul@nandhakumar.net>
2026-05-19 00:54:15 -0700

    Finished adding changes to supermode run as well, testing now with old design to see if it works

--- coupler_switch/coupler_switch_supermode_run.py
+++ coupler_switch/coupler_switch_supermode_run.py
@@ -3,6 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import sys
 from coupler_switch_supermode import run_single
+from coupler_switch_phase_correction import run_coupling_phase_correction
 from coupler_switch_config import WG_COUPLING_CONFIG
 import copy
 sys.path.append(r"C:\Program Files\Lumerical\v202\api\python")
@@ -54,11 +55,19 @@ elif coupling_direction == "vertical":
             r"./lumerical/mode/"
             r"coupler_switch_verticalcpl_toppcm_supermode.lsf"
         )
+
     elif pcm_loading_direction == "side_pcm":
         script_path = (
             r"./lumerical/mode/"
             r"coupler_switch_verticalcpl_sidepcm_supermode.lsf"
         )
+
+    elif pcm_loading_direction == "asym_pcm":
+        script_path = (
+            r"./lumerical/mode/"
+            r"coupler_switch_verticalcpl_asympcm_supermode.lsf"
+        )
+
     else:
         raise ValueError(
             f'Unknown PCM loading direction: '
@@ -94,28 +103,36 @@ coupling_direction = WG_COUPLING_CONFIG["coupling_direction"]
 
 if coupling_direction == "lateral":
 
-    antisym_config["x_coupler_center"] = -(antisym_config["W"]/2 + antisym_config["g"]/2)
-    antisym_config["x_bus_center"] = (antisym_config["W"]/2 + antisym_config["g"]/2)
+    antisym_config["x_coupler_center"] = -(antisym_config["W_coupler"]/2 + antisym_config["g"]/2)
+    antisym_config["x_bus_center"] = (antisym_config["W_bus"]/2 + antisym_config["g"]/2)
 
     antisym_config["y_coupler_center"] = 0
     antisym_config["y_bus_center"] = 0
 
-    sym_config["x_coupler_center"] = -(sym_config["W"]/2 + sym_config["g"]/2)
-    sym_config["x_bus_center"] = (sym_config["W"]/2 + sym_config["g"]/2)
+    sym_config["x_coupler_center"] = -(sym_config["W_coupler"]/2 + sym_config["g"]/2)
+    sym_config["x_bus_center"] = (sym_config["W_bus"]/2 + sym_config["g"]/2)
 
     sym_config["y_coupler_center"] = 0
     sym_config["y_bus_center"] = 0
 
 elif coupling_direction == "vertical":
 
-    antisym_config["y_coupler_center"] = (antisym_config["H"]/2 + antisym_config["g"]/2)
-    antisym_config["y_bus_center"] = -(antisym_config["H"]/2 + antisym_config["g"]/2)
+    H_bus_antisym = antisym_config["H_bus"]
+    H_coupler_antisym = antisym_config["H_coupler"]
+
+    center_sep_antisym = (H_bus_antisym/2 + antisym_config["g"] + H_coupler_antisym/2)
+    antisym_config["y_coupler_center"] = center_sep_antisym / 2
+    antisym_config["y_bus_center"] = -(center_sep_antisym / 2)
 
     antisym_config["x_coupler_center"] = 0
     antisym_config["x_bus_center"] = 0
 
-    sym_config["y_coupler_center"] = (sym_config["H"]/2 + sym_config["g"]/2)
-    sym_config["y_bus_center"] = -(sym_config["H"]/2 + sym_config["g"]/2)
+    H_bus_sym = sym_config["H_bus"]
+    H_coupler_sym = sym_config["H_coupler"]
+
+    center_sep_sym = (H_bus_sym/2 + sym_config["g"] + H_coupler_sym/2)
+    sym_config["y_coupler_center"] = center_sep_sym / 2
+    sym_config["y_bus_center"] = -(center_sep_sym / 2)
 
     sym_config["x_coupler_center"] = 0
     sym_config["x_bus_center"] = 0
@@ -157,12 +174,62 @@ for key, value in sym.items():
 
 if antisym and sym:
 
-    # DESIGN LENGTH (from symmetric state)
-    L = np.pi / (2 * sym["Omega"])   # meters
+    # ============================================================
+    # DETERMINE DESIGN STATE
+    # ============================================================
+
+    if sym["D"] <= antisym["D"]:
+        design_state = sym
+        design_state_name = "sym"
+    else:
+        design_state = antisym
+        design_state_name = "antisym"
+
+    print(f"\nDesign state = {design_state_name}")
+
+    # ============================================================
+    # PHASE-CORRECTED COUPLING LENGTH
+    # ============================================================
+
+    phase_adjustment = run_coupling_phase_correction(
+        lum_project=supermode,
+        lsf_script=coupler_switch_supermode_script,
+
+        coupling_direction=WG_COUPLING_CONFIG["coupling_direction"],
+
+        polarization=WG_COUPLING_CONFIG["polarization"],
+
+        W_bus=WG_COUPLING_CONFIG["W_bus"],
+        H_bus=WG_COUPLING_CONFIG["H_bus"],
+
+        W_coupler=WG_COUPLING_CONFIG["W_coupler"],
+        H_coupler=WG_COUPLING_CONFIG["H_coupler"],
+
+        g=WG_COUPLING_CONFIG["g"],
+
+        Omega=design_state["Omega"],
+
+        R=15e-6
+    )
+
+    theta_tail = phase_adjustment["theta_tail_rad"]
+
+    # Coupling length correction in meters
+    L = phase_adjustment["Lc_corrected_um"] * 1e-6
 
     # ACTUAL POWER TRANSFER
-    P_antisym = (1 - antisym["D"]**2) * np.sin(antisym["Omega"] * L)**2
-    P_sym  = (1 - sym["D"]**2)  * np.sin(sym["Omega"]  * L)**2
+    theta_antisym = antisym["Omega"] * L + theta_tail
+    theta_sym = sym["Omega"] * L + theta_tail
+
+    P_antisym = (1 - antisym["D"]**2) * np.sin(theta_antisym)**2
+    P_sym = (1 - sym["D"]**2) * np.sin(theta_sym)**2
+    
+    # Include coupling loss from imaginary part of effective index
+    alpha_antisym = antisym["loss_eff"] * 100 / (10 * np.log10(np.e))
+    alpha_sym = sym["loss_eff"] * 100 / (10 * np.log10(np.e))
+
+    P_antisym *= np.exp(-alpha_antisym * L)
+    P_sym *= np.exp(-alpha_sym * L)
 
     # avoid log(0)
     P_sym_safe = max(P_sym, 1e-12)

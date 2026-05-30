cd77dad301b1deb88644233d628521b84f98beba
nakulnandhakumar <nakul@nandhakumar.net>
2026-05-29 21:24:20 -0700

    Added bend radius to coupler design analysis files

--- coupler_switch/coupler_switch_design_analysis.py
+++ coupler_switch/coupler_switch_design_analysis.py
@@ -23,19 +23,22 @@ W_coupler_nm = 350
 H_coupler_nm = 450
 
 g_min_nm = 200
-g_max_nm = 300
+g_max_nm = 400
 
-t_pcm_min_nm = 10
-t_pcm_max_nm = 40
+t_pcm_min_nm = 5
+t_pcm_max_nm = 50
 
 t_gap_min_nm = 0
-t_gap_max_nm = 20
+t_gap_max_nm = 50
 
 polarization = "TM" # "TE" or "TM"
+
+bend_radius_um = 15
 
 filename = (
     f"design_"
     f"{polarization}_"
+    f"R{bend_radius_um}um_"
     f"Wbus{W_bus_nm}nm_"
     f"Hbus{H_bus_nm}nm_"
     f"Wcpl{W_coupler_nm}nm_"
@@ -45,26 +48,24 @@ filename = (
     f"tgap{t_gap_min_nm}-{t_gap_max_nm}nm.csv"
 )
 
+# Load the CSV into a DataFrame
 csv_path = os.path.join(design_dir, filename)
-
 df = pd.read_csv(csv_path)
 
-# =====================
-# FILTER DESIGNS
-# Keep only ER >= 20 dB
-# =====================
-#filtered = df[df["ER_dB"] >= 20]
+# filter designs by eliminating those with ER <= 20, then by highest ER
+filtered = df[df["ER_dB"] >= 20]
+filtered = filtered.sort_values(by="ER_dB", ascending=False)
 
-# =====================
-# SORT BY COUPLING LENGTH
-# Smallest coupling length first
-# =====================
-filtered = df.sort_values(by="ER_dB", ascending=False)
+# filter out designs with any gap between pcm and bus waveguide
+filtered = filtered[filtered["t_gap_pcm_nm"] == 0]
+
+# filter out designs with a coupling length greater than 50 microns
+filtered = filtered[filtered["L_corrected_design_um"] <= 50]
 
 # =====================
 # SELECT TOP N
 # =====================
-top_n = 20
+top_n = 30
 top = filtered.head(top_n)
 
 # =====================
